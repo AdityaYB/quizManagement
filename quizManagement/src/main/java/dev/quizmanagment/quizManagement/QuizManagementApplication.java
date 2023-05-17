a960486559cbde32577f4363d76540197bf400ff
AdityaYB <adityaybendale@gmail.com>
2023-05-17 23:20:26 +0530

    project_start

---
+++ quizManagement/src/main/java/dev/quizmanagment/quizManagement/QuizManagementApplication.java
@@ -0,0 +1,13 @@
+package dev.quizmanagment.quizManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizManagementApplication.class, args);
+	}
+
+}
---
+++ quizManagement/src/test/java/dev/quizmanagment/quizManagement/QuizManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.quizmanagment.quizManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
