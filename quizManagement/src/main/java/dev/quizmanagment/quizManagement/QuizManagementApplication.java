c1290bd0461a438e01426eb1489e6d8424bb95bd
AdityaYB <adityaybendale@gmail.com>
2023-05-18 00:09:40 +0530

    back

--- quizManagement/src/main/java/dev/quizmanagment/quizManagement/QuizManagementApplication.java
+++ quizManagement/src/main/java/dev/quizmanagment/quizManagement/QuizManagementApplication.java
@@ -2,19 +2,13 @@ package dev.quizmanagment.quizManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class QuizManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuizManagementApplication.class, args);
 	}
-	@GetMapping("/")
-	public String apiRoot() {
-		return "helloe";
-	}
+
 
 }
